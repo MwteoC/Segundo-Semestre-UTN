5663a35f97c355c7481c7266f26a204e101e97f6
Mateo <135768887+MwteoC@users.noreply.github.com>
2023-08-28 20:46:12 -0300

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -57,3 +57,31 @@ print(nombres)
 # Eliminar la lista
 del nombres
 print(nombres)
+
+# Definimos una tupla
+cocina = ('cuchara', 'cuchillo', 'tenedor')
+print(len(cocina))
+
+# Acceder a un elemento, para esto utilizamos corchetes no parentesis
+print(cocina[0])
+# Mostrar de manera inversa
+print(cocina[-1])
+
+# Acceder a un rango
+print(cocina[0:2])
+
+# Ejemplo
+verduras = ('papa',) # Una tupla necesita aunque sea un elemento la coma
+# De lo contrario seria un tipo str cadena
+
+# Recorremos los elementos de la tupla 
+for cocinar in cocina: # Print usta usando \n para saltos de lineas
+    print(cocinar, end=' ') # Usamos end= para eliminar los saltos de linea
+
+cocinaLista = list(cocina)
+cocinaLista[0] = 'Plato'
+cocina = tuple(cocinaLista)
+
+print('\n', cocina)
+
+# del cocina esto es para eliminar la tupla
